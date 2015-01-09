48257f1e5d4538b57d853dc167f115b3458ae151
cmccully <cmccully@physics.rutgers.edu>
2015-01-08 16:56:22 -0800

    Initial commit with full astropy structure.

--- pygfit/__init__.py
+++ pygfit/__init__.py
@@ -12,4 +12,8 @@ from ._astropy_init import *
 
 # For egg_info test builds to pass, put package imports here.
 if not _ASTROPY_SETUP_:
-    from example_mod import *
+    import pygfit,gblend,models,pygsti
+    slop = pygsti.slop
+    pygsti = pygsti.pygsti
+    pygfit = pygfit.pygfit
+__all__ = ["pygfit","gblend","models","pygsti","slop"]
--- python/__init__.py
+++
@@ -1,7 +0,0 @@
-import pygfit,gblend,models,pygsti
-
-__all__ = ["pygfit","gblend","models","pygsti","slop"]
-
-slop = pygsti.slop
-pygsti = pygsti.pygsti
-pygfit = pygfit.pygfit
